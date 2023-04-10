6a9db6a5c0240eb195a9028e03d433c0ef415a4d
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2023-04-10 11:47:36 +0530

    chore: initial code

---
+++ jsnnce/script.js
@@ -0,0 +1,7 @@
+let socials_button = document.getElementById("socials_button");
+let socials = document.getElementById("socials");
+
+socials_button.addEventListener("click", () => {
+  socials.style.display = "block";
+  console.log("hi");
+});
